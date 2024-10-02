6320543cacbb1765145313f111126cc883297c28
Boris Belyaev <bbor1974@gmail.com>
2024-10-02 17:03:43 +0400

    Fix properties

--- omni_d10f.mk
+++ omni_d10f.mk
@@ -4,6 +4,6 @@ $(call inherit-product, device/jsr/d10f/device.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d10f
 PRODUCT_NAME := omni_d10f
-PRODUCT_BRAND := HighScreen
-PRODUCT_MANUFACTURER := HighScreen
-PRODUCT_MODEL := Boost IIse
+PRODUCT_BRAND := JSR
+PRODUCT_MANUFACTURER := JSR Tech
+PRODUCT_MODEL := D10F
